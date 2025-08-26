553b5e9c4c6ff80335a597bb9b58831d967d3559
hadardgerassi-debug <hadardgerassi@gmail.com>
2025-08-26 12:11:29 +0300

    Create mintlify.config.js

---
+++ mintlify.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  sidebar: [
+    {
+      label: 'Getting Started',
+      pages: [
+        'introduction.md',
+        'setup.md'
+      ]
+    },
+    {
+      label: 'Support Topics',
+      pages: [
+        {
+          label: 'Account Management',
+          pages: [
+            'creating-an-account.md',
+            'resetting-password.md'
+          ]
+        },
+        {
+          label: 'Billing',
+          pages: [
+            'pricing.md',
+            'invoices.md'
+          ]
+        }
+      ]
+    },
+    {
+      label: 'FAQs',
+      file: 'faq.md'
+    }
+  ],
+  title: 'Test Help Center',
+  description: 'Support documentation for testing subcategories',
+};
